ada31408536ee2e9b0265a39ba21b1b6e7553531
mibrito1 <mirian.unasp@gmail.com>
2023-06-28 17:03:10 -0300

    exercicios de typescript-II adicionando algo no array

--- build/index.js
+++ build/index.js
@@ -1,6 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const database_1 = require("./database");
-const database_2 = require("./database");
-console.log(database_2.produtos, database_1.usuarios);
+console.table((0, database_1.getAllUsers)());
+console.table((0, database_1.getAllProducts)());
+(0, database_1.createUser)("u003", "Ciquilana", "ciquilana@gmail.com", "cici1234");
+(0, database_1.createProducts)("prod003", "mouse pad", 55, "mouse pad com apoio ergonomico preto", "https://fotos.oceanob2b.com/High/042195.jpg?ims=150x");
+console.table((0, database_1.getAllUsers)());
+console.table((0, database_1.getAllProducts)());
+console.table((0, database_1.procurarProdutoPorNome)("mouse"));
 //# sourceMappingURL=index.js.map
